5f02b21da8785699bc4204f14455845534c5faab
Jim Belcher <jbelcher1985@gmail.com>
2018-03-23 19:26:17 -0400

    change test runner

--- src/test/java/com/cardstox/cards/CardsApplicationTests.java
+++ src/test/java/com/cardstox/cards/CardsApplicationTests.java
@@ -2,10 +2,11 @@ package com.cardstox.cards;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class CardsApplicationTests {
 
